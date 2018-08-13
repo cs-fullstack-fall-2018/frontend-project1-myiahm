a324efea01baa1e0cdc66027969f6414cc551999
Myiah Moore <myiahsade@gmail.com>
2018-08-12 23:46:00 -0500

    check in

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
 var i = 0;
-var txt = " Hey Nice to meet you. What is your name?";
+var txt = "Nice to meet you. What is your name?";
 var speed = 50; /* The speed/duration of the effect in milliseconds */
-var input = document.getElementById("myWords").value;
 
+printWriter();
 function typeWriter()
 {
 
@@ -10,23 +10,33 @@ function typeWriter()
         document.getElementById("myWords").innerHTML += txt.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
-        object.onsubmit = printWriter();
+        // var input = document.getElementById("VisitorName").value;
+
+        // object.onsubmit = printWriter();
 
 
     }
 
 }
-var p = 0;
-var txt2 = " Hello"+ input + "thank you for visiting my page. If you would like to see more hit enter!. ";
-var speed2 = 50; /* The speed/duration of the effect in milliseconds */
+
+
+
 function printWriter()
 {
+    var input = document.getElementById("VisitorName").value;
+    alert(input);
 
+    var p = 0;
+    var txt2 = " Hello"+ input + "thank you for visiting my page. If you would like to see more hit enter!. ";
+
+    var speed2 = 50; /* The speed/duration of the effect in milliseconds */
     if (p < txt2.length)
     {
         document.getElementById("myWords2").innerHTML += txt2.charAt(p);
         p++;
-        setTimeout(typeWriter,speed2);
+
+        setTimeout(printWriter,speed2);
+
 
 
     }
