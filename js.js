628513012e959621752764bb3aed5316aec95925
Myiah Moore <myiahsade@gmail.com>
2018-08-14 09:55:42 -0500

    check in

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = "Nice to meet you. What is your name?";
+var txt = "Nice to meet you. What is your name?";  //variable that when called gives message
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 printWriter();
@@ -9,7 +9,7 @@ function typeWriter()
     if (i < txt.length) {
         document.getElementById("myWords").innerHTML += txt.charAt(i);
         i++;
-        setTimeout(typeWriter, speed);
+        setTimeout(typeWriter, speed);     // if statement that gets the txt variable to display across the screen at a set speed
         // var input = document.getElementById("VisitorName").value;
 
         // object.onsubmit = printWriter();
@@ -21,10 +21,10 @@ function typeWriter()
 
 
 
-function printWriter()
+function printWriter() ///NEW FUNCTION THAT DISPLAYS A NEW MESSAGE WITH THE USER INPUT IN IT ALONG WITH ANOTHER IF STATEMENT TO MAKE IT DISPLAY ON THE SAME PAGE
 {
     var input = document.getElementById("VisitorName").value;
-    alert(input);
+
 
     var p = 0;
     var txt2 = " Hello"+ input + "thank you for visiting my page. If you would like to see more hit enter!. ";
